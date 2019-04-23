ff39aae19d1d9d0928d02b009dac73b6fadfc155
LeslieWilson <leslie.wilson.vt@gmail.com>
2019-04-22 21:48:41 -0400

    finished python app w/out oop

--- Bank.py
+++ Bank.py
@@ -6,10 +6,12 @@ def withdraw(input, amount):
         savings = open("savings.txt", 'r').read()
         savings = int(savings)
         savings -= amount
+        file = open("savings.txt", "w").write(str(savings))
     elif input == "checking":
         checking = open("checking.txt", 'r').read()
         checking = int(checking)
         checking -= amount
+        file = open("checking.txt", "w").write(str(checking))
 
 
 def deposit(input, amount):
@@ -17,10 +19,12 @@ def deposit(input, amount):
         savings = open("savings.txt", 'r').read()
         savings = int(savings)
         savings += amount
+        file = open("savings.txt", "w").write(str(savings))
     elif input == "checking":
-        checking = open("checking.tandhxt",'r').read()
+        checking = open("checking.txt",'r').read()
         checking = int(checking)
         checking += amount
+        file = open("checking.txt", "w").write(str(checking))
 
 def transfer (input, amount):
     if input == "savings":
@@ -28,26 +32,20 @@ def transfer (input, amount):
         savings = int(savings)
         savings -= amount
         file = open("savings.txt", "w").write(str(savings))
-
-
-
-
-
-
-
-
-
-
         checking = open("checking.txt", 'r').read()
         checking = int(checking)
         checking += amount
+        file = open("checking.txt", "w").write(str(checking))
+
     elif input == "checking":
-        checking = ("checking.txt", 'r').read()
+        checking = open("checking.txt", 'r').read()
         checking = int(checking)
         checking -= amount
-        savings = open ("savings.txt",'r').read()
+        file = open("checking.txt",'w').write(str(checking))
+        savings = open("savings.txt","r").read()
         savings = int(savings)
-        saving += amount
+        savings += amount
+        file = open("savings.txt", "w").write(str(savings))
 
 
 print("what account do you want to use")
@@ -55,25 +53,23 @@ account = input()
 print("ok, what do you want to do? choose transfer, deposit, withdraw")
 action = input()
 print("how much money do you want to move? give an integer")
-amount = (int(input())
+amount = input()
+amount = (int(amount))
 
 
 if action == "transfer" and account == "savings":
-    transfer("savings", amount)
+    transfer(account, amount)
 elif action == "transfer" and account == "checking":
-    transfer("checking", amount)
+    transfer(account, amount)
 
+elif account == 'savings' and action == "deposit":
+    deposit(account,amount)
 
+elif account == 'savings' and action == "withdraw":
+    withdraw(account,amount)
 
+elif account == "checking" and action == "deposit":
+    deposit(account,amount)
 
-
-
-
-
-#         File.open("Bank.txt", 'w')
-#         opened_file= file.write(savings)
-#     elif input == "checking":
-#         checking -= amount
-#
-#
-# withdraw("savings", 40)
+elif account == "checking" and action == "withdraw":
+    withdraw(account,amount)
